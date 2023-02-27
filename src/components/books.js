6106d9b2ba2eb74681b74c8ed4af77bb416275fd
Muhammad Talha Hanif <talhahanif5666@gmail.com>
2023-02-27 13:15:07 +0500

    Initial commit

--- src/components/books.js
+++ src/components/books.js
@@ -2,7 +2,7 @@ import React from 'react';
 import '../App.css';
 import { v4 as uuidv4 } from 'uuid';
 import AddnewBook from './Form';
-import Book from './list';
+import Book from './booklist';
 
 const Books = () => {
   const book = [
