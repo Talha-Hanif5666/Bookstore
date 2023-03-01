9dd68825969846c6e0d71efb337de47eb5f6df18
Muhammad Talha Hanif <talhahanif5666@gmail.com>
2023-03-01 16:32:03 +0500

    Refactor componets features

--- src/components/books.js
+++ src/components/books.js
@@ -1,11 +1,15 @@
-import React from 'react';
-import '../App.css';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import AddnewBook from './Form';
 import Book from './booklist';
+import { getBooks } from '../redux/books/bookSlice';
 
 const Books = () => {
   const book = useSelector((state) => state.books);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getBooks());
+  }, [dispatch]);
 
   return (
     <div className="book-container">
